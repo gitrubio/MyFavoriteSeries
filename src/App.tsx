68499030332f53df859a7664a3870010e317e6c8
carlos rubio <86839953+gitrubio@users.noreply.github.com>
2023-04-17 11:56:39 -0500

    add : formato para las series v1

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Row , Card} from 'antd'
+import { Row , Card, Col} from 'antd'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Row>
-      <Card hoverable style={{width : 500, height: 500}}>
-
-      </Card>
+    <Row gutter={[16, 16]} wrap >
+    <Col xs={24} sm={12} md={12} lg={8} xl={8} xxl={6}>
+    <Card hoverable style={{width : 400, height: 400}}
+    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+    />
+    </Col>
     </Row>
   )
 }
