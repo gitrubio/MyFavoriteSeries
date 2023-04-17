28e19a8a7ef4cb9919b40436acb4ccb445c59d9f
carlos rubio <86839953+gitrubio@users.noreply.github.com>
2023-04-17 17:21:51 -0500

    add : dividiendo componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
-import { Row, Card, Col } from "antd";
+import { Row, Card, Col, Drawer } from "antd";
 function App() {
-  const [count, setCount] = useState(0);
+  const [open, setOpen] = useState(false);
 
   return (
     <Row gutter={[16, 16]} wrap justify={"start"}>
+        <Drawer title="Basic Drawer" placement="right" onClose={()=>setOpen(false)} open={open}>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Drawer>
       <Col xs={24} sm={12} md={12} lg={8} xl={8} xxl={6}>
-        <Card title='lucifer' hoverable style={{ width: 350}}>
+        <Card title='Stranger Things' hoverable style={{ width: 350}}>
           <article>
             <img src={"https://firebasestorage.googleapis.com/v0/b/mypricelist-7e6fe.appspot.com/o/strangerFont.jpg?alt=media&token=d008d37b-e8b2-489d-83dd-5da74d9d83f8"} />
             <img src={"https://firebasestorage.googleapis.com/v0/b/mypricelist-7e6fe.appspot.com/o/eleven.png?alt=media&token=3155baef-c5cb-413d-aa2b-1b5cf3e6898b"} />
@@ -15,9 +20,9 @@ function App() {
         </Card>
       </Col>
       <Col xs={24} sm={12} md={12} lg={8} xl={8} xxl={6}>
-        <Card title='lucifer' hoverable style={{ width: 350 ,height: 550 }}>
+        <Card title='You' hoverable style={{ width: 350 ,height: 550 }}>
           <article>
-            <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/es-la-you-s3-main-best-lies-vertical-27x40-rgb-pre-1633522084.jpg" />
+            <img src="https://firebasestorage.googleapis.com/v0/b/mypricelist-7e6fe.appspot.com/o/you.jpg?alt=media&token=23a7b971-b765-4ae1-ba5f-5a7e77e79127" />
             <img src={"https://firebasestorage.googleapis.com/v0/b/mypricelist-7e6fe.appspot.com/o/joe.png?alt=media&token=57cb3d29-51e0-444c-9de3-503600f3e910"} />
           </article>
           <Card.Meta title="Europe Street beat" description="www.instagram.com" />
@@ -32,17 +37,17 @@ function App() {
         </Card>
       </Col>
       <Col xs={24} sm={12} md={12} lg={8} xl={8} xxl={6}>
-        <Card title='lucifer' hoverable style={{ width: 350 ,height: 550 }}>
+        <Card title='Merlina' hoverable style={{ width: 350 ,height: 550 }}>
           <article>
-            <img src="https://i0.wp.com/cuatrobastardos.com/wp-content/uploads/2022/09/Merlina-Poster.jpg?resize=603%2C892&ssl=1" />
+            <img src="https://firebasestorage.googleapis.com/v0/b/mypricelist-7e6fe.appspot.com/o/Merlina-Poster.webp?alt=media&token=89941325-a511-484e-a9b5-8ec28b373312" />
             <img src={"https://candybargratis.com/wp-content/uploads/2022/12/merlina-personaje-png-imprimible.png"} />
           </article>
         </Card>
       </Col>
       <Col xs={24} sm={12} md={12} lg={8} xl={8} xxl={6}>
-        <Card title='lucifer' hoverable style={{ width: 350 ,height: 550 }}>
+        <Card title='Dark' hoverable style={{ width: 350 ,height: 550 }}>
           <article>
-            <img src="https://es.web.img3.acsta.net/r_1280_720/pictures/19/06/11/21/33/0075880.jpg" />
+            <img src="https://firebasestorage.googleapis.com/v0/b/mypricelist-7e6fe.appspot.com/o/dark.jpg?alt=media&token=e00cf3b0-76c4-43ac-95ca-e07334cae95d" />
             <img src={"https://firebasestorage.googleapis.com/v0/b/mypricelist-7e6fe.appspot.com/o/jonasxdark.png?alt=media&token=74f4ac9f-f429-473e-8f98-7d0e13daf011"} />
           </article>
         </Card>
@@ -50,7 +55,7 @@ function App() {
       <Col xs={24} sm={12} md={12} lg={8} xl={8} xxl={6}>
         <Card title='lucifer' hoverable style={{ width: 350 ,height: 550 }}>
           <article>
-            <img src="https://m.media-amazon.com/images/M/MV5BM2IwNWY2YWEtNTU4Ni00MmE2LTljZjItNWQ0NzBlNjJiMzBiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_FMjpg_UX1000_.jpg" />
+            <img src="https://firebasestorage.googleapis.com/v0/b/mypricelist-7e6fe.appspot.com/o/dhamerFont.jpg?alt=media&token=613062a2-aefd-4e08-8e6e-c20c367800af" />
             <img src={"https://firebasestorage.googleapis.com/v0/b/mypricelist-7e6fe.appspot.com/o/dahamer-removebg-preview.png?alt=media&token=37aa0a3e-787c-4c4a-8f29-5c68ba088094"} />
           </article>
         </Card>
---
+++ src/interfaces/index.ts
@@ -0,0 +1,8 @@
+export interface IProsCard {
+    showDrawer : () => void;
+    title: string;
+    description : string
+    characters : string[]
+    imgFont : string
+    mainCharacterImg : string
+}
