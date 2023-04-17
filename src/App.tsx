60000096045b628e1939180bf6f3276d08af61b7
carlos rubio <86839953+gitrubio@users.noreply.github.com>
2023-04-17 14:39:41 -0500

    add : subiendo imagenes a firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import { useState } from "react";
 import { Row, Card, Col } from "antd";
-import strangetThings from './assets/strangerFont.jpg'
-import eleven from "./assets/eleven.png"
-import joe from './assets/joe.png'
 function App() {
   const [count, setCount] = useState(0);
 
@@ -11,8 +8,8 @@ function App() {
       <Col xs={24} sm={12} md={12} lg={8} xl={8} xxl={6}>
         <Card hoverable style={{ width: 350}}>
           <article>
-            <img src={strangetThings} />
-            <img src={eleven} />
+            <img src={"https://firebasestorage.googleapis.com/v0/b/mypricelist-7e6fe.appspot.com/o/strangerFont.jpg?alt=media&token=d008d37b-e8b2-489d-83dd-5da74d9d83f8"} />
+            <img src={"https://firebasestorage.googleapis.com/v0/b/mypricelist-7e6fe.appspot.com/o/eleven.png?alt=media&token=3155baef-c5cb-413d-aa2b-1b5cf3e6898b"} />
           </article>
           <Card.Meta title="Europe Street beat" description="www.instagram.com" />
         </Card>
@@ -21,7 +18,7 @@ function App() {
         <Card hoverable style={{ width: 350 ,height: 550 }}>
           <article>
             <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/es-la-you-s3-main-best-lies-vertical-27x40-rgb-pre-1633522084.jpg" />
-            <img src={joe} />
+            <img src={"https://firebasestorage.googleapis.com/v0/b/mypricelist-7e6fe.appspot.com/o/joe.png?alt=media&token=57cb3d29-51e0-444c-9de3-503600f3e910"} />
           </article>
           <Card.Meta title="Europe Street beat" description="www.instagram.com" />
         </Card>
