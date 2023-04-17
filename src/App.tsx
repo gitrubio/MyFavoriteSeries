87c6b1395be9854d2d3f70750099d6f2a8c27a93
carlos rubio <86839953+gitrubio@users.noreply.github.com>
2023-04-17 14:21:49 -0500

    add : nuevas cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import { Row, Card, Col } from "antd";
-
+import strangetThings from './assets/strangerFont.jpg'
+import eleven from "./assets/eleven.png"
+import joe from './assets/joe.png'
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <Row gutter={[16, 16]} wrap justify={"space-between"}>
       <Col xs={24} sm={12} md={12} lg={8} xl={8} xxl={6}>
-        <Card hoverable style={{ width: 350 }}>
+        <Card hoverable style={{ width: 350}}>
           <article>
-            <img src="https://genotipia.com/wp-content/uploads/2017/10/stranger-things-season-2-poster-2-786x1174.jpg" />
-            <img src="https://64.media.tumblr.com/2e5c2f919d32e28cbacc06ab87fca595/tumblr_ozw3biKUfq1vl7x77o1_1280.png" />
+            <img src={strangetThings} />
+            <img src={eleven} />
+          </article>
+          <Card.Meta title="Europe Street beat" description="www.instagram.com" />
+        </Card>
+      </Col>
+      <Col xs={24} sm={12} md={12} lg={8} xl={8} xxl={6}>
+        <Card hoverable style={{ width: 350 ,height: 550 }}>
+          <article>
+            <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/es-la-you-s3-main-best-lies-vertical-27x40-rgb-pre-1633522084.jpg" />
+            <img src={joe} />
           </article>
           <Card.Meta title="Europe Street beat" description="www.instagram.com" />
         </Card>
